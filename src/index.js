8275b4b2ac3ad026807edfff6a21ab9de0ec81ee
mfxy <mfxyzm@outlook.com>
2018-06-28 23:46:37 +1000

    Import axios and add URL

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
@@ -6,10 +7,11 @@ import 'antd/dist/antd.css'
 import './styles';
 import App from './App';
 
+axios.defaults.baseURL = 'http://lmsdemomar.azurewebsites.net/api/';
 
 ReactDOM.render(
 	<Router>
-	<App />
-	</ Router>,
+		<App />
+	</Router>,
 	document.getElementById('root'),
 );
