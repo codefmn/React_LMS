1a479341d688df5f33634381c3dbd2e257624677
mfxy <mfxyzm@outlook.com>
2018-06-28 23:47:15 +1000

    Update Routes

--- src/App/Routes.js
+++ src/App/Routes.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import {Route, Redirect} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import CoursesView from '../Course/CoursesView';
 import CourseDetailView from '../Course/CourseDetailView';
 import LecturersView from '../Lecturer/LecturersView';
 import LecturerDetailView from '../Lecturer/LecturerDetailView';
+import LecturerEditView from '../Lecturer/LecturerEditView';
 
 export default () => ((
   <div>
@@ -12,5 +13,6 @@ export default () => ((
     <Route exact path="/courses/:id" component={CourseDetailView} />
     <Route exact path="/lecturers" component={LecturersView} />
     <Route exact path="/lecturers/:id" component={LecturerDetailView} />
+    <Route exact path="/lecturers/edit/:id" component={LecturerEditView} />
   </div>
 ))
