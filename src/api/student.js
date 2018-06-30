4f50bff0b57d8b1fed3da7a9cbfc2d89a0fb9641
mfxy <mfxyzm@outlook.com>
2018-06-30 14:28:58 +1000

    Add APIs

---
+++ src/api/student.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export function fetchStudents(){
+    return axios.get('/student');
+}
+
+export function fetchStudentById(id){
+    return axios.get(`/student/${id}`);
+}
+
+export function saveStudent(data){
+    return axios.post('/student',data);
+}
+
+export function updateStudent(id, data){
+    return axios.put(`/student/${id}`, data);
+}
