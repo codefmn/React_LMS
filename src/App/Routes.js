a972a8d0d3a1245faf9b6f33ddb590e94c50da49
mfxy <mfxyzm@outlook.com>
2018-07-09 01:16:15 +1000

    Update Routes

--- src/App/Routes.js
+++ src/App/Routes.js
@@ -2,6 +2,10 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 import CoursesView from '../Course/CoursesView';
 import CourseDetailView from '../Course/CourseDetailView';
+import CourseEditView from '../Course/CourseEditView';
+import StudentsView from '../Student/StudentsView';
+import StudentDetailView from '../Student/StudentDetailView';
+import StudentEditView from '../Student/StudentEditView';
 import LecturersView from '../Lecturer/LecturersView';
 import LecturerDetailView from '../Lecturer/LecturerDetailView';
 import LecturerEditView from '../Lecturer/LecturerEditView';
@@ -11,6 +15,10 @@ export default () => ((
     <Route exact path="/" component={CoursesView}/>
     <Route exact path="/courses" component={CoursesView} />
     <Route exact path="/courses/:id" component={CourseDetailView} />
+    <Route exact path="/courses/edit/:id" component={CourseEditView} />
+    <Route exact path="/students" component={StudentsView} />
+    <Route exact path="/students/:id" component={StudentDetailView} />
+    <Route exact path="/students/edit/:id" component={StudentEditView} />
     <Route exact path="/lecturers" component={LecturersView} />
     <Route exact path="/lecturers/:id" component={LecturerDetailView} />
     <Route exact path="/lecturers/edit/:id" component={LecturerEditView} />
